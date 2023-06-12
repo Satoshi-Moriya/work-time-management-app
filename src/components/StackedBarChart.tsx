57c848e890e9b86f6ca1e972823bb1adb52844f0
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-12 10:06:31 +0900

    表の見た目を作った #27

---
+++ src/components/StackedBarChart.tsx
@@ -0,0 +1,15 @@
+// type StackedBarChartProps = {
+//   icon: React.JSX.Element;
+//   text: string;
+//   url: string;
+// }
+
+// const StackedBarChart: React.FC<StackedBarChartProps> = ({ icon, text, url }) => {
+const StackedBarChart = () => {
+
+  return (
+    <div>棒グラフ</div>
+  );
+}
+
+export default StackedBarChart;
