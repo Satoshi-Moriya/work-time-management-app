1cbaf03c8987be264178e6c49fbd704156856e6c
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-13 19:17:27 +0900

    勤怠グラフを表示できるようにした #34

--- src/components/StackedBarChart.tsx
+++ src/components/StackedBarChart.tsx
@@ -1,15 +1,33 @@
-// type StackedBarChartProps = {
-//   icon: React.JSX.Element;
-//   text: string;
-//   url: string;
-// }
+import React from 'react';
 
-// const StackedBarChart: React.FC<StackedBarChartProps> = ({ icon, text, url }) => {
-const StackedBarChart = () => {
+type TimeRange = {
+  start: number;
+  end: number;
+};
+
+type StackedBarChartProps = {
+  timeData: TimeRange[];
+};
+
+const DATE_SECONDS = 60 * 60 * 24; // 86400秒
+const CHART_WIDTH = 800;
+
+const StackedBarChart: React.FC<StackedBarChartProps> = ({timeData}) => {
 
   return (
-    <div>棒グラフ</div>
+    <>
+      {timeData.map((time, index) => (
+        <div
+          key={index}
+          className="bg-[#BAD3FF] h-8 absolute inline-block top-1/2 -translate-y-1/2"
+          style={{
+            left: `${(time.start / DATE_SECONDS) * 100}%`,
+            width: `${((time.end - time.start) / DATE_SECONDS) * CHART_WIDTH}px`
+          }}
+        ></div>
+      ))}
+    </>
   );
-}
+};
 
 export default StackedBarChart;
