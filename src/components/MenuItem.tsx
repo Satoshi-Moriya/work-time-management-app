06c7300fac8461e87a5bbaea5a48e9dcb098ef51
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-15 13:29:36 +0900

    NavLinkを使って対応した #28

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const DEFAULT_LINK_PROPERTIES = "flex items-center py-1 px-4 hover:bg-dark-gray";
+const ACTIVE_LINK_PROPERTIES = "flex items-center py-1 px-4 bg-dark-gray font-bold";
 
 type MenuItemProps = {
   icon: React.JSX.Element;
@@ -10,10 +13,13 @@ const MenuItem: React.FC<MenuItemProps> = ({ icon, text, url }) => {
 
   return (
     <li>
-      <Link to={url} className="flex items-center py-1 px-4 hover:bg-dark-gray focus:bg-dark-gray">
+      <NavLink
+        to={url}
+        className={({ isActive }) => isActive ? ACTIVE_LINK_PROPERTIES : DEFAULT_LINK_PROPERTIES}
+      >
         {icon}
         <span className="ml-2">{text}</span>
-      </Link>
+      </NavLink>
     </li>
   );
 }
