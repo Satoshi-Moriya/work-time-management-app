62a01e38d33e1f807f586ac0c6da05a3204fd158
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-17 18:37:45 +0900

    zodを使ってバリデーションを設定した #47

--- src/page/ChangeMail.tsx
+++ src/page/ChangeMail.tsx
@@ -1,4 +1,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { changeMailValidationSchema } from "../utils/validationSchema";
 
 // 現在のパスワード（DBを参照して取得してくる事になるはず）
 const nowMailAddress = "nowMailAddress@gmail.com";
@@ -9,21 +12,29 @@ type FormValues = {
 }
 
 const ChangeMail = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm<FormValues>({defaultValues: { mail: nowMailAddress }});
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm<FormValues>({
+    defaultValues: { mail: nowMailAddress },
+    mode: "onChange",
+    resolver: zodResolver(changeMailValidationSchema),
+  });
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
-  
+
   return (
     <div className="p-10">
       <h3 className="text-l font-bold">メールアドレス変更</h3>
       <form onSubmit={handleSubmit(onSubmit)} className="mt-8">
         <div>
           <label htmlFor="password">パスワード</label>
-          <input type="password" id="password" placeholder="パスワード" {...register("password", {required: "パスワードを入力してください。"})}  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
+          <input type="password" id="password" placeholder="パスワード" {...register("password")} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
           <p className="text-red-500">{errors.password && errors.password.message}</p>
         </div>
         <div className="mt-4">
           <label htmlFor="mail">メールアドレス</label>
-          <input type="email" id="mail" placeholder="メールアドレス" {...register("mail", {required: "メールアドレスを入力してください。"})}  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 w-full rounded-md sm:text-sm"/>
+          <input type="email" id="mail" placeholder="メールアドレス" {...register("mail")} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 w-full rounded-md sm:text-sm"/>
           <p className="text-red-500">{errors.mail && errors.mail.message}</p>
         </div>
         <div className="mt-8">
--- src/page/ChangePassword.tsx
+++ src/page/ChangePassword.tsx
@@ -1,13 +1,24 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { changePasswordValidationSchema } from "../utils/validationSchema";
+
 
 type FormValues = {
-  nowPassword: string;
+  currentPassword: string;
   newPassword: string;
-  confirmNowPassword: string;
+  confirmNewPassword: string;
 }
 
 const ChangePassword = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm<FormValues>({ mode: "onChange" });
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm<FormValues>({
+    mode: "onChange",
+    resolver: zodResolver(changePasswordValidationSchema)
+  });
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
   return (
@@ -15,19 +26,20 @@ const ChangePassword = () => {
       <h3 className="text-l font-bold">パスワード変更</h3>
       <form onSubmit={handleSubmit(onSubmit)} className="mt-8">
         <div>
-          <label htmlFor="nowPassword">現在のパスワード</label>
-          <input type="password" id="nowPassword" placeholder="パスワード" {...register("nowPassword", {required: "パスワードを入力してください。"})} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
-          <p className="text-red-500">{errors.nowPassword && errors.nowPassword.message}</p>
+          <label htmlFor="currentPassword">現在のパスワード</label>
+          <input type="password" id="currentPassword" placeholder="パスワード" {...register("currentPassword")} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
+          <p className="text-red-500">{errors.currentPassword && errors.currentPassword.message}</p>
         </div>
         <div className="mt-4">
           <label htmlFor="newPassword">新しいパスワード</label>
-          <input type="password" id="newPassword" placeholder="新しいパスワード" {...register("newPassword", {required: "パスワードを入力してください。"})} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
+          <p className="mt-1 text-sm text-slate-400">※半角英数字8文字以上16文字以下</p>
+          <input type="password" id="newPassword" placeholder="新しいパスワード" {...register("newPassword")} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
           <p className="text-red-500">{errors.newPassword && errors.newPassword.message}</p>
         </div>
         <div className="mt-4">
-          <label htmlFor="confirmNowPassword">新しいパスワード（確認）</label>
-          <input type="password" id="confirmNowPassword" placeholder="新しいパスワード（確認）" {...register("confirmNowPassword", {required: "パスワードを入力してください。"})} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
-          <p className="text-red-500">{errors.confirmNowPassword && errors.confirmNowPassword.message}</p>
+          <label htmlFor="confirmNewPassword">新しいパスワード（確認）</label>
+          <input type="password" id="confirmNewPassword" placeholder="新しいパスワード（確認）" {...register("confirmNewPassword")} className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 block w-full rounded-md sm:text-sm"/>
+          <p className="text-red-500">{errors.confirmNewPassword && errors.confirmNewPassword.message}</p>
         </div>
         <div className="mt-8">
           <button type="submit" className="bg-orange-400 hover:bg-orange-700 focus:bg-orange-700 border-orange-400 rounded-lg text-white font-bold px-3 py-2">変更する</button>
