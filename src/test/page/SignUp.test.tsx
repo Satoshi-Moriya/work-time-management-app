75e0c30c1e19498786bf0d8f39fd63a3ada33a4e
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-20 12:04:38 +0900

    テストの漏れがあったので追加した #65

--- src/test/page/SignUp.test.tsx
+++ src/test/page/SignUp.test.tsx
@@ -105,6 +105,146 @@ describe("SingUpページの単体テスト", () => {
     })();
   });
 
+  describe("メールアドレスのバリデーションチェック", () => {
+
+    test("必須のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const emailInputEl = screen.getByPlaceholderText('メールアドレス');
+
+      // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+      await act(async() => {
+        await user.type(emailInputEl, "email@test.com");
+      });
+      user.clear(emailInputEl);
+
+      const emailNoEmptyErrorMessageEl = await screen.findByText("メールアドレスは必須です。");
+      expect(emailNoEmptyErrorMessageEl).toBeInTheDocument();
+    });
+
+    test("形式のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const emailInputEl = screen.getByPlaceholderText('メールアドレス');
+
+      user.type(emailInputEl, "email");
+
+      const emailFormatErrorMessageEl = await screen.findByText("メールアドレスが正しい形式ではありません。");
+      expect(emailFormatErrorMessageEl).toBeInTheDocument();
+    });
+  });
+
+  describe("パスワードのバリデーションチェック", () => {
+
+    test("必須のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const passwordInputEl = screen.getByPlaceholderText("パスワード");
+
+      // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+      await act(async() => {
+        await user.type(passwordInputEl, "test1234");
+      });
+      user.clear(passwordInputEl);
+
+      const passwordNoEmptyErrorMessageEl = await screen.findByText("パスワードは必須です。");
+      expect(passwordNoEmptyErrorMessageEl).toBeInTheDocument();
+    });
+
+    test("最低文字数のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const passwordInputEl = screen.getByPlaceholderText("パスワード");
+
+      user.type(passwordInputEl, "test123");
+
+      const passwordMinErrorMessageEl = await screen.findByText("パスワードは8文字以上で入力してください。");
+      expect(passwordMinErrorMessageEl).toBeInTheDocument();
+
+      // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+      await act(async() => {
+        await user.clear(passwordInputEl);
+        await user.type(passwordInputEl, "test1234");
+      });
+
+      expect(passwordMinErrorMessageEl).not.toBe("パスワードは8文字以上で入力してください。");
+    });
+
+    test("最高文字数のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const passwordInputEl = screen.getByPlaceholderText('パスワード');
+
+      user.type(passwordInputEl, "test1234test1234t");
+
+      const passwordMaxErrorMessageEl = await screen.findByText("パスワードは16文字以下で入力してください");
+      expect(passwordMaxErrorMessageEl).toBeInTheDocument();
+
+      // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+      await act(async() => {
+        await user.clear(passwordInputEl);
+        await user.type(passwordInputEl, "test1234test1234");
+      });
+
+      expect(passwordMaxErrorMessageEl).not.toBe("パスワードは16文字以下で入力してください");
+    });
+
+    test("半角英数字混合のバリデーションチェック", async() => {
+      const user = userEvent.setup();
+      render(<SingUp />);
+      const passwordInputEl = screen.getByPlaceholderText('パスワード');
+
+      user.type(passwordInputEl, "testtest");
+
+      const passwordHalfWidthAlphanumericErrorMessageEl = await screen.findByText("パスワードは半角英数字混合で入力してください");
+      expect(passwordHalfWidthAlphanumericErrorMessageEl).toBeInTheDocument();
+
+      // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+      await act(async() => {
+        await user.clear(passwordInputEl);
+        await user.type(passwordInputEl, "test1234");
+      });
+
+      expect(passwordHalfWidthAlphanumericErrorMessageEl).not.toBe("パスワードは半角英数字混合で入力してください");
+    });
+  });
+
+  test("パスワード（確認）のバリデーションチェック", async() => {
+    const user = userEvent.setup();
+    render(<SingUp />);
+    const confirmPasswordInputEl = screen.getByPlaceholderText("パスワード（確認）");
+
+    // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+    await act(async() => {
+      await user.type(confirmPasswordInputEl, "test");
+      user.clear(confirmPasswordInputEl);
+    });
+
+    const passwordNoEmptyErrorMessageEl = await screen.findByText("パスワード（確認）は必須です。");
+    expect(passwordNoEmptyErrorMessageEl).toBeInTheDocument();
+  });
+
+  test("パスワードとパスワード（確認）の一致チェック", async() => {
+    const user = userEvent.setup();
+    render(<SingUp />);
+    const passwordInputEl = screen.getByPlaceholderText("パスワード");
+    const confirmPasswordInputEl = screen.getByPlaceholderText("パスワード（確認）");
+
+    user.type(passwordInputEl, "test1234");
+    user.type(confirmPasswordInputEl, "test123");
+
+    const passwordNotMatchErrorMessageEl = await screen.findByText("パスワードが一致しません");
+    expect(passwordNotMatchErrorMessageEl).toBeInTheDocument();
+
+    // ToDo ここをactで囲まなければいけない理由がいまいちわからない
+    await act(async() => {
+      await user.clear(confirmPasswordInputEl);
+      await user.type(confirmPasswordInputEl, "test1234");
+    });
+
+    expect(passwordNotMatchErrorMessageEl).not.toBe("パスワードが一致しません");
+  });
+
   // ToDo バックエンドと連携させるときな気がする（？）
   test.skip("フォームのバリデーションチェック", async() => {
     const user = userEvent.setup();
