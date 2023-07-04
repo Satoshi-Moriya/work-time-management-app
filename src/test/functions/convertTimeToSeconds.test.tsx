ed69727452c667ff4e8f489403c477405ca28667
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-07-04 11:02:09 +0900

    hookでなくなった関数のテストをrenderhookなしにした #90

--- src/test/functions/convertTimeToSeconds.test.tsx
+++ src/test/functions/convertTimeToSeconds.test.tsx
@@ -1,30 +1,16 @@
-import { renderHook } from "@testing-library/react";
-
 import convertTimeToSeconds from "../../functions/convertTimeToSeconds"
 
 describe("convertTimeToSecondsの単体テスト", () => {
 
   test("「00:mm:ss」が秒数に変換されている", () => {
-    const dateTimeString = "2023-06-29 00:01:59";
-    const { result } = renderHook(() =>
-      convertTimeToSeconds(dateTimeString)
-    );
-    expect(result.current).toBe(119);
+    expect(convertTimeToSeconds("2023-06-29 00:01:59")).toBe(119);
   });
 
   test("「0H:mm:ss」が秒数に変換されている", () => {
-    const dateTimeString = "2023-06-29 09:00:59";
-    const { result } = renderHook(() =>
-      convertTimeToSeconds(dateTimeString)
-    );
-    expect(result.current).toBe(32459);
+    expect(convertTimeToSeconds("2023-06-29 09:00:59")).toBe(32459);
   });
 
   test("「HH:mm:ss」が秒数に変換されている", () => {
-    const dateTimeString = "2023-06-29 13:15:00";
-    const { result } = renderHook(() =>
-      convertTimeToSeconds(dateTimeString)
-    );
-    expect(result.current).toBe(47700);
+    expect(convertTimeToSeconds("2023-06-29 13:15:00")).toBe(47700);
   });
 });
