a99aa08acb94825af5b5060a6ec357e26d2f7a1a
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-07-04 11:25:35 +0900

    hookでなくなった関数のテストをrenderhookなしにした #91

--- src/test/functions/getLastDayOfMonth.test.tsx
+++ src/test/functions/getLastDayOfMonth.test.tsx
@@ -1,5 +1,3 @@
-import { renderHook } from "@testing-library/react";
-
 import getLastDayOfMonth from "../../functions/getLastDayOfMonth"
 
 describe("getLastDayOfMonthの単体テスト", () => {
@@ -7,36 +5,24 @@ describe("getLastDayOfMonthの単体テスト", () => {
   test("年月の最終日の取得ができる。（2023年6月）", () => {
     const selectedYear = 2023;
     const selectedMonth = 6;
-    const { result } = renderHook(() =>
-      getLastDayOfMonth(selectedYear, selectedMonth)
-    );
-    expect(result.current).toBe(30);
+    expect(getLastDayOfMonth(selectedYear, selectedMonth)).toBe(30);
   })
 
   test("年月の最終日の取得ができる。（2023年7月）", () => {
     const selectedYear = 2023;
     const selectedMonth = 7;
-    const { result } = renderHook(() =>
-      getLastDayOfMonth(selectedYear, selectedMonth)
-    );
-    expect(result.current).toBe(31);
+    expect(getLastDayOfMonth(selectedYear, selectedMonth)).toBe(31);
   })
 
   test("年月の最終日の取得ができる。（2023年2月）", () => {
     const selectedYear = 2023;
     const selectedMonth = 2;
-    const { result } = renderHook(() =>
-      getLastDayOfMonth(selectedYear, selectedMonth)
-    );
-    expect(result.current).toBe(28);
+    expect(getLastDayOfMonth(selectedYear, selectedMonth)).toBe(28);
   })
 
   test("年月の最終日の取得ができる。（閏年の2024年2月）", () => {
     const selectedYear = 2024;
     const selectedMonth = 2;
-    const { result } = renderHook(() =>
-      getLastDayOfMonth(selectedYear, selectedMonth)
-    );
-    expect(result.current).toBe(29);
+    expect(getLastDayOfMonth(selectedYear, selectedMonth)).toBe(29);
   })
 })
