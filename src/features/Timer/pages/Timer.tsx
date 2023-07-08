17ba886f1640a423f1738a4b3cc234ec8a10360e
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-07-08 12:27:37 +0900

    ストップウォッチのリアルタイム更新を実現するためにWebSocketの実験 #100

--- src/features/Timer/pages/Timer.tsx
+++ src/features/Timer/pages/Timer.tsx
@@ -1,9 +1,11 @@
-import StopWatch from '../components/StopWatch';
+// import StopWatch from '../components/StopWatch';
+import ChatRoom from '../components/ChatRoom';
 
 const Timer = () => {
   return (
       <main className="grow flex flex-col items-center justify-center ml-48 h-screen">
-        <StopWatch />
+        {/* <StopWatch /> */}
+        <ChatRoom />
       </main>
   );
 }
