6ac04d23cb8e50df69399b592344d5cb63ebd40a
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-07-13 18:12:45 +0900

    ストップウォッチが稼働中にページ遷移をすると確認モーダルが出るようになった #101

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Layout from "../components/Layout";
 import Login from '../features/Login/pages/Login';
 import Timer from "../features/Timer/pages/Timer";
-import WorKLog from "../features/WorkLog/pages/WorkLog";
+import WorkLog from "../features/WorkLog/pages/WorkLog";
 import Setting from "../features/Setting/pages/Setting";
 import ChangeEmail from "../features/ChangeEmail/pages/ChangeEmail";
 import ChangePassword from "../features/ChangePassword/pages/ChangePassword";
@@ -14,25 +14,35 @@ import SignUp from "../features/SignUp/pages/SignUp";
 import PreRegistrationComplete from "../features/SignUp/pages/PreRegistrationComplete";
 import ResetPasswordSubmitted from "../features/ResetPassword/pages/ResetPasswordSubmitted";
 
+export const router = createBrowserRouter([
+  { path: "login", element: <Login /> },
+  { path: "resetpassword", element: <ResetPassword /> },
+  { path: "signup", element: <SignUp /> },
+  { path: "preregistrationcomplete", element: <PreRegistrationComplete /> },
+  { path: "resetpasswordsubmitted", element: <ResetPasswordSubmitted /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Timer /> },
+      { path: "worklog", element: <WorkLog /> },
+      {
+        path: "setting",
+        element: <Setting />,
+        children: [
+          { path: "changeemail", element: <ChangeEmail /> },
+          { path: "changepassword", element: <ChangePassword /> },
+          { path: "cancel", element: <Cancel /> },
+        ],
+      },
+    ],
+  },
+  { path: "*", element: <Page404 /> },
+]);
+
 const Router = () => {
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="resetpassword" element={<ResetPassword />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="preregistrationcomplete" element={<PreRegistrationComplete />} />
-      <Route path="resetpasswordsubmitted" element={<ResetPasswordSubmitted />} />
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Timer />}/>
-        <Route path="worklog" element={<WorKLog />}/>
-        <Route path="setting" element={<Setting />}>
-          <Route path="changeemail" element={<ChangeEmail />} />
-          <Route path="changepassword" element={<ChangePassword />} />
-          <Route path="cancel" element={<Cancel />} />
-        </Route>
-      </Route>
-      <Route path="/*" element={<Page404 />}/>
-    </Routes>
+    <RouterProvider router={router} />
   );
 }
 
