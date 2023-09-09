5b07eb995feec7d8d422c6930de798bd2c32d5fc
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-09-09 17:03:17 +0900

    RecordItemLogの機能のファイル分割 #191

---
+++ src/features/RecordItemLog/repositories/fetchRecordItemLogs.ts
@@ -0,0 +1,15 @@
+import { api } from "../../../lib/api-client/ApiClientProvider";
+import { RecordItemLogType } from "../types";
+
+export const fetchRecordItemLogs = async(
+  selectedRecordItemId: number,
+  selectedMonthFrom: string,
+  selectedMonthTo: string
+) => {
+  return await api.get<RecordItemLogType[]>(`/record-item-logs/${selectedRecordItemId}`, {
+    params: {
+      from: selectedMonthFrom,
+      to: selectedMonthTo
+    }
+  });
+}
