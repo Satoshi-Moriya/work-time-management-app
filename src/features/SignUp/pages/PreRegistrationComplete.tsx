02059ab530734f9ebe9631c80986463bd5d7318e
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-09-12 12:52:54 +0900

    ユーザー登録完了ページのレスポンシブ #194

--- src/features/SignUp/pages/PreRegistrationComplete.tsx
+++ src/features/SignUp/pages/PreRegistrationComplete.tsx
@@ -5,12 +5,12 @@ const PreRegistrationComplete = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="p-10">
-        <h1 className="text-7xl text-center">登録完了</h1>
+      <h1 className="text-center text-4xl md:text-7xl">登録完了</h1>
         <div className="mt-16">
           <p className="text-lg text-center">ユーザー登録が完了しました。</p>
         </div>
         <div className="mt-8 text-center">
-          <Link to="../login" relative="path" className="text-2xl underline text-blue-500 hover:opacity-70">ログインページへ戻る</Link>
+          <Link to="../login" relative="path" className="text-xl md:text-2xl underline text-blue-500 hover:opacity-70">ログインページへ戻る</Link>
         </div>
       </div>
     </div>
