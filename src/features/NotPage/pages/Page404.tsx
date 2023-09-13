8ca77d9c81f7d34162ff80224f68c0b55f9846f9
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-09-13 23:38:56 +0900

    404ページのレスポンシブ #194

--- src/features/NotPage/pages/Page404.tsx
+++ src/features/NotPage/pages/Page404.tsx
@@ -1,9 +1,9 @@
 const Page404 = () => {
 
   return (
-    <div className="grow flex pl-48 h-screen">
+    <div className="grow flex md:pl-48 mt-52 md:mt-0 h-screen">
       <div className="py-10 px-7 h-full">
-        <h2 className="text-3xl font-bold">404</h2>
+        <h2 className="text-2xl md:text-3xl font-bold">404</h2>
         <p className="mt-8"> こちらのページ存在しません。</p>
       </div>
     </div>
