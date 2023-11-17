f55853e26e12864088ee26cb906b0762633ca722
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-11-17 18:47:37 +0900

    退会を不可にした(フロントのみ）#230

--- src/features/Cancel/hooks/useCancel.ts
+++ src/features/Cancel/hooks/useCancel.ts
@@ -18,14 +18,15 @@ export const useCancel = (): [
   const accountDeleteHandler = async() => {
     const confirm = window.confirm("本当にアカウントを削除してもよろしいですか？");
     if (confirm) {
-      try {
-        const response = await deleteUser(userId);
-        setToast({message: response.data.message, isSuccess: true});
-        setUserId(null);
-        navigate("/login");
-      } catch(error) {
-        setToast({message: "予期せぬ問題が発生し、アカウントを削除できませんでした。時間をおいて再度お試しください。", isSuccess: false});
-      }
+      // try {
+      //   const response = await deleteUser(userId);
+      //   setToast({message: response.data.message, isSuccess: true});
+      //   setUserId(null);
+      //   navigate("/login");
+      // } catch(error) {
+      //   setToast({message: "予期せぬ問題が発生し、アカウントを削除できませんでした。時間をおいて再度お試しください。", isSuccess: false});
+      // }
+      setToast({message: "このアカウントは削除できませんでした。", isSuccess: false});
     }
   }
 
